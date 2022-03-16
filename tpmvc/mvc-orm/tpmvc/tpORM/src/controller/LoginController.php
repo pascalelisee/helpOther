7f6136d8af7e603740fa384625a33edcb17bd44f
pascalelisee <80065473+pascalelisee@users.noreply.github.com>
2022-03-16 16:26:43 +0100

    first commit

---
+++ tpmvc/mvc-orm/tpmvc/tpORM/src/controller/LoginController.php
@@ -0,0 +1,68 @@
+<?php
+// namespace src\controller;
+use libs\system\Controller;
+use src\model\UserDB;
+
+class LoginController extends Controller
+{
+    private $userdao;
+    public function __construct() {
+        parent::__construct();
+        //$this->userdao = new UserDB();
+    }
+    /**
+     * http://localhost/tpmvc/tpORM/Login/index
+     */
+    public function index() {
+        
+        return $this->view->load("accueil");
+    }
+    /**
+     * 
+     * http://localhost/tpmvc/tpORM/Login/logon
+     */
+  /*   public function logon() {
+        // $userdao;
+        extract($_POST);
+        $users = $this->userdao->getConnection($email, md5($password));
+        if (count($users) != 0) 
+        {
+            $user = $users[0];
+            return $this->index ();
+        } else {
+            return $this->login ();
+        }
+    }  */
+
+    /**
+     * http://localhost/tpmvc/tpORM/Login/login
+     */
+    public function login () {
+
+        return $this->view->load("login");
+    }
+    /**
+     * http://localhost/tpmvc/tpORM/Login/logout
+     */
+    public function logout() {
+
+    }
+
+    //
+
+     public function Admin() {
+        return $this ->view -> load("admin");
+    }
+
+    public function logon() {
+        extract($_POST);
+        $users = $this->userdao->getConnection($email, md5($password));
+        if (count($users) != 0) 
+        {
+            $user = $users[0];
+            return $this->index ();
+        } else {
+            return $this->login ();
+        }
+    } 
+}
---
+++ tpmvc/mvc-orm/tpmvc/tpORM/src/model/UserDB.php
@@ -0,0 +1,24 @@
+<?php
+namespace src\model;
+use libs\system\Model;
+use User;
+
+class UserDB extends Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+  
+    public function getConnection($email, $password){
+      
+      return $this->entityManager
+                    ->createQuery("SELECT u FROM User u WHERE u.email = :em
+                                    AND u.password = :pass")
+                    ->setParameter('em', $email)
+                    ->setParameter('pass', $password)
+                    ->getResult(); //array(Object)
+        
+    }
+
+}
